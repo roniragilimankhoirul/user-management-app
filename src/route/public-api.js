f9bb2e57eb42e663d26204d5de984ac8076ed1e6
roniragilimankhoirul <ronisensei7@gmail.com>
2023-09-14 19:54:48 +0700

    add OpenAPI SwaggerUi Documentation

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,9 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swagger from "../../docs/api.json" assert { type: "json" };
 const publicRouter = new express.Router();
 
 publicRouter.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello DOTS",
+    message: "Hello DOT",
   });
 });
+publicRouter.use("/api/docs", swaggerUi.serve);
+publicRouter.get("/api/docs", swaggerUi.setup(swagger));
 export { publicRouter };
